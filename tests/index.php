012b5bb0a5935e1daf1d0262c8f7b36d807e414c
bnomei <b@bnomei.com>
2023-10-22 13:49:21 +0100

    :rocket: Ready for Kirby 4
    Signed-off-by: bnomei <b@bnomei.com>

--- tests/index.php
+++ tests/index.php
@@ -1,5 +1,7 @@
 <?php
 
 const KIRBY_HELPER_DUMP = false;
-require_once __DIR__.'/../vendor/autoload.php';
+const KIRBY_HELPER_E = false;
+
+require __DIR__.'/../vendor/autoload.php';
 echo (new Kirby())->render();
